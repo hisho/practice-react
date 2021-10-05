585ec2e30d9795ba65095c829e129f1825039ba3
hisho <hisho.web@gmail.com>
2021-10-06 00:06:55 +0900

    ✨ add couner app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import './app.css'
+import { useState } from 'react'
+
 function App() {
-  return <div className="App">なんか書きます</div>
+  const [count, setCount] = useState(0)
+  console.log(count)
+
+  //増えるは5で止まる
+  //減るは0で止まる
+  return (
+    <div>
+      <div className="App">{count}</div>
+      <button
+        disabled={count >= 5}
+        type={'button'}
+        onClick={() => {
+          setCount((prevState) => prevState + 1)
+        }}
+      >
+        増える
+      </button>
+      <button
+        disabled={count <= 0}
+        type={'button'}
+        onClick={() => {
+          setCount((prevState) => prevState - 1)
+        }}
+      >
+        減る
+      </button>
+      <button
+        type={'button'}
+        onClick={() => {
+          setCount(0)
+        }}
+      >
+        リセット
+      </button>
+    </div>
+  )
 }
 
 export default App
