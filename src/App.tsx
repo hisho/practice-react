58d630ea39261e28064b192ad1d5e32b698c7433
hisho <hisho.web@gmail.com>
2021-10-13 00:05:04 +0900

    🎉 swiper

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-function App() {
+import { memo, useMemo, useRef, useState } from 'react'
+
+const slides = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4']
+
+export const App = memo(() => {
+  //ここを教える
+  const [index, setIndex] = useState(0)
+  //ここを教える
+  const ref = useRef<HTMLDivElement | null>(null)
+  //ここはuseStateやuseEffectにする
+  const width = useMemo(() => {
+    return ref.current?.clientWidth ?? 0
+  }, [ref])
+
   return (
     <div className="swiper">
       <div className="swiper-container">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide is-active">
-            <div className="content">Slide 1</div>
-          </div>
-          <div className="swiper-slide">
-            <div className="content">Slide 2</div>
-          </div>
-          <div className="swiper-slide">
-            <div className="content">Slide 3</div>
-          </div>
-          <div className="swiper-slide">
-            <div className="content">Slide 4</div>
-          </div>
+        <div
+          className="swiper-wrapper"
+          ref={ref}
+          style={{
+            transform: `translate3d(${width * index * -1}px, 0, 0)`,
+          }}
+        >
+          {slides.map((slide, slideIndex) => (
+            <div
+              className={'swiper-slide'}
+              key={`swiper-slide_${slide}_${slideIndex}`}
+            >
+              <div className="content">{slide}</div>
+            </div>
+          ))}
         </div>
-        <button type={'button'} disabled className="swiper-button-prev" />
-        <button type={'button'} className="swiper-button-next" />
+        <button
+          type={'button'}
+          className="swiper-button-prev"
+          disabled={index <= 0}
+          onClick={() => setIndex((prevState) => prevState - 1)}
+        />
+        <button
+          type={'button'}
+          className="swiper-button-next"
+          disabled={index >= slides.length - 1}
+          onClick={() => setIndex((prevState) => prevState + 1)}
+        />
         <div className="swiper-pagination">
-          <button
-            type={'button'}
-            className="swiper-pagination-bullet is-active"
-          />
-          <button type={'button'} className="swiper-pagination-bullet" />
-          <button type={'button'} className="swiper-pagination-bullet" />
-          <button type={'button'} className="swiper-pagination-bullet" />
+          {slides.map((slide, slideIndex) => (
+            <button
+              key={`swiper-pagination_${slide}_${slideIndex}`}
+              type={'button'}
+              className={`swiper-pagination-bullet ${
+                slideIndex === index ? 'is-active' : ''
+              }`}
+              onClick={() => setIndex(slideIndex)}
+            />
+          ))}
         </div>
       </div>
     </div>
   )
-}
-
-export default App
+})
