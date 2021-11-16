487a3801b0f7e8f9436154080aa2701fb7a20c6e
hisho <hisho.web@gmail.com>
2021-11-16 22:29:47 +0900

    ♻️ logicとviewを切り離し

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-import { useState } from 'react'
-import { v4 } from 'uuid'
+import { useTodo } from './useTodo'
 
 function App() {
-  const [inputText, setInputText] = useState('')
-  const [todoList, setTodoList] = useState<
-    {
-      uuid: string
-      todo: string
-    }[]
-  >([])
-
-  //削除するメソッド
-  const onDelete = (uuid: string) => {
-    setTodoList((prevState) => {
-      return prevState.filter((n) => n.uuid !== uuid)
-    })
-  }
-
-  const onAdd = () => {
-    if (inputText.length === 0) return
-    setTodoList((prevState) => [
-      ...prevState,
-      {
-        uuid: v4(),
-        todo: inputText,
-      },
-    ])
-    setInputText('')
-  }
+  const { inputText, onChangeInputText, onAdd, todoList, onDelete } = useTodo()
 
   return (
     <div style={{ width: '30rem', margin: '0 auto' }}>
@@ -36,23 +10,21 @@ function App() {
         type="text"
         name={'todo'}
         value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
+        onChange={onChangeInputText}
       />
       <button type={'button'} onClick={onAdd}>
         追加
       </button>
       <div>
         <ul>
-          {todoList.map((item, index) => {
-            return (
-              <li key={`todo_${index}`}>
-                {item.todo}
-                <button type={'button'} onClick={() => onDelete(item.uuid)}>
-                  削除
-                </button>
-              </li>
-            )
-          })}
+          {todoList.map(({ todo, uuid }) => (
+            <li key={`todo_${uuid}`}>
+              {todo}
+              <button type={'button'} onClick={() => onDelete(uuid)}>
+                削除
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/useTodo.ts
@@ -0,0 +1,43 @@
+import { ChangeEvent, useState } from 'react'
+import { v4 } from 'uuid'
+
+export const useTodo = () => {
+  const [inputText, setInputText] = useState('')
+  const [todoList, setTodoList] = useState<
+    {
+      uuid: string
+      todo: string
+    }[]
+  >([])
+
+  const onChangeInputText = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value)
+  }
+
+  const onAdd = () => {
+    if (inputText.length === 0) return
+    setTodoList((prevState) => [
+      ...prevState,
+      {
+        uuid: v4(),
+        todo: inputText,
+      },
+    ])
+    setInputText('')
+  }
+
+  //削除するメソッド
+  const onDelete = (uuid: string) => {
+    setTodoList((prevState) => {
+      return prevState.filter((n) => n.uuid !== uuid)
+    })
+  }
+
+  return {
+    inputText,
+    onChangeInputText,
+    onAdd,
+    todoList,
+    onDelete,
+  }
+}
