867267adaf199657309ac480e9a42bccd81d6ffe
hisho <hisho.web@gmail.com>
2021-11-30 22:35:05 +0900

    ✨ udpate App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from 'react'
+import { instance } from './libs/axios'
+import { requests } from './libs/request'
+
+type Movie = {
+  id: string
+  name: string
+  title: string
+  original_name: string
+  poster_path: string
+  backdrop_path: string
+}
+
 function App() {
-  return <div className="App">なんか書きます</div>
+  const [movies, setMovies] = useState<Movie[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await instance.get<{ results: Movie[] }>(
+        requests.fetchTrending
+      )
+      setMovies(res.data.results)
+    })()
+  }, [])
+
+  console.log(movies)
+  return (
+    <div className="App">
+      {movies.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <div>{movie.title}</div>
+            <img
+              src={'https://image.tmdb.org/t/p/original' + movie.backdrop_path}
+              alt=""
+            />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App
